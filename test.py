bccfa21140acf38dc633e8b5cea84d96c09528fa
Martin Ueding <dev@martin-ueding.de>
2012-02-12 21:47:07 +0100

    Change

--- test.py
+++ test.py
@@ -1,21 +1,36 @@
 from epydoc.markup import epytext
+from epydoc import docparser
 import subprocess
 
-def test():
-    """
-    Test::
+__doc__ = """
+Test::
 
-        Indented paragraph
+    Indented paragraph
 
-    C{code} and B{bold}
-    """
+C{code} and B{bold}
+"""
+
+print __doc__
+
+def foo():
     pass
 
-doc = test.__doc__
-dom = epytext.parse_docstring(doc, [])
-latex = dom.to_latex(None)
+if __name__ == '__main__':
+    filename = "test.py"
 
-proc = subprocess.Popen(["pandoc", "-r", "latex", "-w", "markdown"], stdin=subprocess.PIPE)
-proc.stdin.write(latex)
-proc.stdin.close()
-proc.communicate()
+    valuedoc = docparser.parse_docs(filename)
+
+    print valuedoc
+    print "---"
+    print valuedoc.summary
+    print "---"
+
+if False:
+
+    dom = epytext.parse_docstring(valuedoc.docstring, [])
+    latex = dom.to_latex(None)
+
+    proc = subprocess.Popen(["pandoc", "-r", "latex", "-w", "markdown"], stdin=subprocess.PIPE)
+    proc.stdin.write(latex)
+    proc.stdin.close()
+    proc.communicate()

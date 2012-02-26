3bfbfaaa4779c87815ce888304e676e82a432952
Martin Ueding <dev@martin-ueding.de>
2012-02-26 16:33:47 +0100

    Error handling, stipping docstring

--- readmeextract.py
+++ readmeextract.py
@@ -15,8 +15,21 @@ import sys
 __docformat__ = "restructuredtext en"
 
 def main():
+    if len(sys.argv) != 2:
+        sys.stderr.write("Usage: python readmeextract.py module\n")
+
+    # Import the given module
     mod = __import__(sys.argv[1])
-    print mod.__doc__
+
+    # Print the stipped docstring
+    print mod.__doc__.strip()
+
+    # Check whether there is a ``__docformat__`` set.
+    try:
+        if not mod.__docformat__.startswith("restructuredtext"):
+            sys.stderr.write("-> This is probably no reST\n")
+    except AttributeError:
+        sys.stderr.write("-> Not sure if that is really reST\n")
 
 if __name__ == '__main__':
     main()
